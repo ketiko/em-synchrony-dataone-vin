b64d5af8b7c22c4c806609fb3e2ae09c11a11734
Tad Thorley <phaedryx@gmail.com>
2014-06-16 12:51:09 -0600

    simplify test (no more hash matching)

--- test/em-synchrony/dataone-vin_test.rb
+++ test/em-synchrony/dataone-vin_test.rb
@@ -4,8 +4,6 @@ require 'em-synchrony/dataone-vin'
 module EventMachine
   module Synchrony
     describe DataoneVin do
-      include TestHelpers
-
       before do
         DataoneVin.configure(*DATAONE_CONFIG)
       end
@@ -13,8 +11,18 @@ module EventMachine
       it 'should work' do
         EM.synchrony do
           result = EM::Synchrony::DataoneVin.get('5TBRT3418YS094830')
-          result['query_responses']['Request-Sample'].delete('transaction_id')
-          result.must_equal expected_result
+
+          result['decoder_messages']['service_provider'].must_equal "DataOne Software, Inc."
+          result['decoder_messages']['decoder_version'].must_equal "7.0.0"
+          result['decoder_messages']['decoder_errors'].must_be_empty
+
+          result['query_responses']['Request-Sample']['query_error']['error_code'].must_be_empty
+          result['query_responses']['Request-Sample']['query_error']['error_message'].must_be_empty
+
+          result['query_responses']['Request-Sample']['common_data']['basic_data']['year'].must_equal "2000"
+          result['query_responses']['Request-Sample']['common_data']['basic_data']['make'].must_equal "Toyota"
+          result['query_responses']['Request-Sample']['common_data']['basic_data']['model'].must_equal "Tundra"
+
           EM.stop
         end
       end
